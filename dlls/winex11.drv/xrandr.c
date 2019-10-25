9a652d913dd1caba6ea00705d9cba605023b5d7e
Zhiyi Zhang <zzhang@codeweavers.com>
2019-10-25 21:39:54 +0800

    winex11.drv: Call XFlush() right after XUngrabServer().
    XGrabServer() stops the processing of other display connections
    until a XUngrabServer() call is actually processed by the X server.
    
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 6dbb153ede48e77a87dddf37e5276276a701c5c3)

--- dlls/winex11.drv/xrandr.c
+++ dlls/winex11.drv/xrandr.c
@@ -422,6 +422,7 @@ static LONG xrandr12_set_current_mode( int mode )
     if (status != RRSetConfigSuccess)
     {
         XUngrabServer( gdi_display );
+        XFlush( gdi_display );
         ERR("Failed to disable CRTC.\n");
         pXRRFreeCrtcInfo( crtc_info );
         pXRRFreeScreenResources( resources );
@@ -443,6 +444,7 @@ static LONG xrandr12_set_current_mode( int mode )
                                 crtc_info->rotation, crtc_info->outputs, crtc_info->noutput );
 
     XUngrabServer( gdi_display );
+    XFlush( gdi_display );
 
     pXRRFreeCrtcInfo( crtc_info );
     pXRRFreeScreenResources( resources );
