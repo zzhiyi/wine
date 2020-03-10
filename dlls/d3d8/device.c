a8a6f605b8fa5b0eb134f5bc13a9e3133f97254f
Zhiyi Zhang <zzhang@codeweavers.com>
2020-03-10 17:30:50 +0800

    wined3d: Pass a struct wined3d_adapter pointer to wined3d_device_create().
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit a8072e0841248b3c9a8e58c3f433df4250217825)

--- dlls/d3d8/device.c
+++ dlls/d3d8/device.c
@@ -3644,7 +3644,9 @@ HRESULT device_init(struct d3d8_device *device, struct d3d8 *parent, struct wine
 {
     struct wined3d_swapchain_desc swapchain_desc;
     struct wined3d_swapchain *wined3d_swapchain;
+    struct wined3d_adapter *wined3d_adapter;
     struct d3d8_swapchain *d3d_swapchain;
+    unsigned int output_idx;
     HRESULT hr;
 
     static const enum wined3d_feature_level feature_levels[] =
@@ -3654,6 +3656,10 @@ HRESULT device_init(struct d3d8_device *device, struct d3d8 *parent, struct wine
         WINED3D_FEATURE_LEVEL_6,
         WINED3D_FEATURE_LEVEL_5,
     };
+
+    output_idx = adapter;
+    if (output_idx >= parent->wined3d_output_count)
+        return D3DERR_INVALIDCALL;
 
     device->IDirect3DDevice8_iface.lpVtbl = &d3d8_device_vtbl;
     device->device_parent.ops = &d3d8_wined3d_device_parent_ops;
@@ -3669,7 +3675,8 @@ HRESULT device_init(struct d3d8_device *device, struct d3d8 *parent, struct wine
     if (!(flags & D3DCREATE_FPU_PRESERVE)) setup_fpu();
 
     wined3d_mutex_lock();
-    if (FAILED(hr = wined3d_device_create(wined3d, adapter, device_type,
+    wined3d_adapter = wined3d_output_get_adapter(parent->wined3d_outputs[output_idx]);
+    if (FAILED(hr = wined3d_device_create(wined3d, wined3d_adapter, device_type,
             focus_window, flags, 4, feature_levels, ARRAY_SIZE(feature_levels),
             &device->device_parent, &device->wined3d_device)))
     {
