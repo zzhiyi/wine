248ff57438661779881faf84222186e0b4102665
Zhiyi Zhang <zzhang@codeweavers.com>
2020-03-10 17:35:35 +0800

    wined3d: Pass a struct wined3d_adapter pointer to wined3d_check_device_format().
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit f2a850c8442fb174f91e1988c04855a4b7ea7720)

--- dlls/d3d9/directx.c
+++ dlls/d3d9/directx.c
@@ -319,8 +319,10 @@ static HRESULT WINAPI d3d9_CheckDeviceFormat(IDirect3D9Ex *iface, UINT adapter, 
     }
 
     wined3d_mutex_lock();
-    hr = wined3d_check_device_format(d3d9->wined3d, adapter, device_type, wined3dformat_from_d3dformat(adapter_format),
-            usage, bind_flags, wined3d_rtype, wined3dformat_from_d3dformat(format));
+    wined3d_adapter = wined3d_output_get_adapter(d3d9->wined3d_outputs[output_idx]);
+    hr = wined3d_check_device_format(d3d9->wined3d, wined3d_adapter, device_type,
+            wined3dformat_from_d3dformat(adapter_format), usage, bind_flags, wined3d_rtype,
+            wined3dformat_from_d3dformat(format));
     wined3d_mutex_unlock();
 
     return hr;
