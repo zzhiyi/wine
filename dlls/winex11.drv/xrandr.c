bd7088527059608c408332d336d81f08ec4d406c
Zhiyi Zhang <zzhang@codeweavers.com>
2020-05-05 10:42:46 +0800

    winex11.drv: Resize desktop upon display change events.
    Otherwise, Wine still has the old desktop size after a user changed
    display resolution using host system utilities.
    
    X11DRV_DisplayDevices_Update() is introduced so that display devices
    reinitialization is separated from desktop resizing. Otherwise,
    X11DRV_DisplayDevices_Init() would have to be called twice to resize
    the desktop with up-to-date primary monitor dimensions, wasting resources.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=48441
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>

--- dlls/winex11.drv/xrandr.c
+++ dlls/winex11.drv/xrandr.c
@@ -261,7 +261,7 @@ static LONG xrandr10_set_current_mode( int mode )
     if (stat == RRSetConfigSuccess)
     {
         xrandr_current_mode = mode;
-        X11DRV_resize_desktop( dd_modes[mode].width, dd_modes[mode].height );
+        X11DRV_DisplayDevices_Update( TRUE );
         return DISP_CHANGE_SUCCESSFUL;
     }
 
@@ -509,7 +509,7 @@ static LONG xrandr12_set_current_mode( int mode )
     }
 
     xrandr_current_mode = mode;
-    X11DRV_resize_desktop( dd_modes[mode].width, dd_modes[mode].height );
+    X11DRV_DisplayDevices_Update( TRUE );
     return DISP_CHANGE_SUCCESSFUL;
 }
 
@@ -1325,7 +1325,12 @@ static void xrandr14_free_monitors( struct x11drv_monitor *monitors )
 static BOOL xrandr14_device_change_handler( HWND hwnd, XEvent *event )
 {
     if (hwnd == GetDesktopWindow() && GetWindowThreadProcessId( hwnd, NULL ) == GetCurrentThreadId())
-        X11DRV_DisplayDevices_Init( TRUE );
+    {
+        /* Don't send a WM_DISPLAYCHANGE message here because this event may be a result from
+         * ChangeDisplaySettings(). Otherwise, ChangeDisplaySettings() would send multiple
+         * WM_DISPLAYCHANGE messages instead of just one */
+        X11DRV_DisplayDevices_Update( FALSE );
+    }
     return FALSE;
 }
 
