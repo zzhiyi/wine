d83342db08361115dd9f8becbb67b7149be6bede
Zhiyi Zhang <zzhang@codeweavers.com>
2021-07-16 17:22:20 +0800

    include: Add more HiDPI properties.
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>

--- include/tmschema.h
+++ include/tmschema.h
@@ -322,6 +322,8 @@ BEGIN_TM_PROPS()
     TM_PROP(2422, TMT, MINDPI3,                   INT)
     TM_PROP(2423, TMT, MINDPI4,                   INT)
     TM_PROP(2424, TMT, MINDPI5,                   INT)
+    TM_PROP(2433, TMT, MINDPI6,                   INT)
+    TM_PROP(2434, TMT, MINDPI7,                   INT)
 
     /* Font rendering properties */
     TM_PROP(2601, TMT, GLYPHFONT,                 FONT)
@@ -335,6 +337,8 @@ BEGIN_TM_PROPS()
     TM_PROP(3006, TMT, IMAGEFILE5,                FILENAME)
     TM_PROP(3007, TMT, STOCKIMAGEFILE,            FILENAME)
     TM_PROP(3008, TMT, GLYPHIMAGEFILE,            FILENAME)
+    TM_PROP(3009, TMT, IMAGEFILE6,                FILENAME)
+    TM_PROP(3010, TMT, IMAGEFILE7,                FILENAME)
 
     /* String rendering properties */
     TM_PROP(3201, TMT, TEXT,                      STRING)
